c37a5e20cfbb311dd288c3a9f9decaba68dd662b
sarvaraj <sarvaraj.singh@infogain.com>
2017-01-13 13:44:50 +0530

    Added stable code

--- src/test/scala/com/dexcom/testCase/TC_327.scala
+++ src/test/scala/com/dexcom/testCase/TC_327.scala
@@ -1,5 +1,8 @@
 package com.dexcom.testCase
 
+import java.text.{ParseException, SimpleDateFormat}
+import java.util.Date
+
 import com.dexcom.common.CassandraQueries
 import com.dexcom.connection.CassandraConnection
 import com.dexcom.dto.{DeviceSummary, EGVForPatientBySystemTime}
@@ -40,6 +43,19 @@ class TC_327 extends FunSuite with CassandraQueries with BeforeAndAfterAll {
     assert(list_device_summary.head.Model === "G5" || list_device_summary.head.Model === "G4")
     // fetch data frm cassandra
 
+  }
+
+  def stringToDate(dateString : String) : Either[Unit, Date]= {
+    val df = new SimpleDateFormat("yyyy'-'MM'-'dd'T'HH':'mm':'ss'.'SSSz") //2014-05-16T20:06:17.1592279Z
+    val dfNew = new SimpleDateFormat("EEE MMM dd HH:mm:ss z yyyy")
+    try {
+      val date = df.parse(dateString)
+      val dateNew = dfNew.parse(dfNew.format(date))
+      Right(dateNew)
+    } catch {
+      case e : ParseException =>
+        Left(e.printStackTrace())
+    }
   }
 
 }
